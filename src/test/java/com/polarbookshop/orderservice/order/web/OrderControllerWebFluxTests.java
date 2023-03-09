33474136a4724de78df33b1ec00a5bb8f8f44f29
jempitman <jempitman@gmail.com>
2023-03-09 14:51:47 +0100

    refactor: authenticate unavailable book order slice test

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.orderservice.order.web;
 
+import com.polarbookshop.orderservice.config.SecurityConfig;
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
@@ -7,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -15,7 +20,11 @@ import static org.mockito.BDDMockito.given;
 
 
 @WebFluxTest(OrderController.class)
+@Import(SecurityConfig.class)
 public class OrderControllerWebFluxTests {
+
+    public static final String ROLE_EMPLOYEE = "ROLE_employee";
+    public static final String ROLE_CUSTOMER = "ROLE_customer";
 
     @Autowired
     private WebTestClient webClient;
@@ -23,15 +32,22 @@ public class OrderControllerWebFluxTests {
     @MockBean
     private OrderService orderService;
 
+    @MockBean
+    ReactiveJwtDecoder reactiveJwtDecoder;
+
     @Test
-    void whenBookNotAvailableThenRejectOrder(){
+    void whenAuthenticatedBookNotAvailableThenRejectOrder(){
         var orderRequest = new OrderRequest("1234567890", 3);
         var expectedOrder = OrderService.buildRejectedOrder(
                 orderRequest.isbn(), orderRequest.quantity());
-        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+        given(orderService.submitOrder(
+                orderRequest.isbn(), orderRequest.quantity()))
                 .willReturn(Mono.just(expectedOrder));
 
         webClient
+                .mutateWith(SecurityMockServerConfigurers
+                        .mockJwt()
+                        .authorities(new SimpleGrantedAuthority(ROLE_CUSTOMER)))
                 .post()
                 .uri("/orders/")
                 .bodyValue(orderRequest)
